f35966e9b469660279360c139e303662b55baeb8
BrunoBerger <bruno.berger@stud.hshl.de>
2021-02-19 03:42:46 +0100

    basic 3D camera follow setup

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿  
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // target is assigned in the editor
+    public GameObject target;
+    private Vector3 targetPos;
+
+    // cam-behaviour
+    public float turnSpeed = 4.0f;
+    public Vector3 camAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      camAngle.Set(0, 3, -7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      targetPos = target.transform.position;
+      camAngle = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed,
+                                        Vector3.up) * camAngle;
+      transform.position = targetPos + camAngle;
+      transform.LookAt(targetPos);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float force = 20f;
+    public float torque;
+    public float maxSpeed = 20f;
+
+    public Rigidbody rb;
+    public Vector3 velocity;
+    private float potGain;
+    public bool isgrounded;
+
+    public Vector3 inputDrive;
+    public float inputTurn;
+
+
+    void Start()
+    {
+      torque = 3f;
+      rb = this.GetComponent<Rigidbody>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      inputDrive = new Vector3(0f,0f,Input.GetAxis("Vertical"));
+      inputTurn = Input.GetAxis("Horizontal");
+      potGain = rb.velocity.magnitude + inputDrive.magnitude;
+    }
+    void FixedUpdate()
+    {
+      if (isgrounded==true)
+      {
+        rb.AddRelativeTorque(0,torque*inputTurn,0,ForceMode.Impulse);
+        if (potGain < maxSpeed)
+        {
+          moveChar(inputDrive);
+        }
+      }
+    }
+
+
+    void moveChar(Vector3 direction)
+    {
+      rb.AddRelativeForce(direction * force);
+    }
+
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+      if (collisionInfo.gameObject.name == "Terrain")
+      {
+        isgrounded = true;
+      }
+    }
+    void OnCollisionExit(Collision collisionInfo)
+    {
+      if (collisionInfo.gameObject.name == "Terrain")
+      {
+        isgrounded = false;
+      }
+    }
+}
