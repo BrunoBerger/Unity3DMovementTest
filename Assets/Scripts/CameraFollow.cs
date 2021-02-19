f8768a6a70f98db704e698d5e04fc5329274b568
BrunoBerger <bruno.berger@stud.hshl.de>
2021-02-19 19:55:52 +0100

    3d animation test

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -16,7 +16,7 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      camAngle.Set(0, 3, -7);
+      camAngle.Set(0, 2, -4);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/playerStates.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStates : MonoBehaviour
+{
+    public Animator animatior;
+    public Rigidbody playerRB;
+
+    public Vector3 playerVelocity;
+    public bool playerOnGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerVelocity= playerRB.velocity;
+        animatior.SetFloat("speed", playerVelocity.magnitude);
+
+        Debug.DrawRay(transform.position, playerVelocity*1000, Color.red);
+    }
+}
