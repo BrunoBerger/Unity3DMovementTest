fe0027ecede293260a27f480406916568033f5f2
BrunoBerger <bruno.berger@stud.hshl.de>
2021-03-05 20:55:04 +0100

    added initial ground-collision with checkSphere()

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,18 @@ public class PlayerMovement : MonoBehaviour
   public Camera playerCam;
   public Animator animatior;
   public CharacterController controller;
+  public LayerMask GroundLayer;
+
 
   public float gravity;
   public float moveSpeed;
   public float jumpHeight;
   public float turnSmoothTime = 0.1f;
   public float turnSmoothVelocity;
-  
-  public bool playerIsGrounded;
 
-  private Vector3 rawDirection;
+  private bool playerIsGrounded;
+
+  public Vector3 rawDirection;
   private Vector3 moveDir;
   private Vector3 playerExtraVelocity;
 
@@ -29,6 +31,21 @@ public class PlayerMovement : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    // Ground-collision-check
+    //get the radius of the players capsule collider, and make it a tiny bit smaller than that
+    float radius = controller.radius * 0.9f;
+    //get the position (assuming its right at the bottom) and move it up by almost the whole radius
+    Vector3 pos = transform.position + Vector3.up*(radius*0.5f);
+    //returns true if the sphere touches something on that layer
+    bool playerIsGrounded = Physics.CheckSphere(pos, radius, GroundLayer);
+    Debug.Log(playerIsGrounded);
+
+    // Add all relevant constant forces on the character
+    playerExtraVelocity.y += gravity * Time.deltaTime;
+    if (playerIsGrounded)
+      playerExtraVelocity.y = 0;
+
+
     // Horizontal Movement
     float h = Input.GetAxis("Horizontal");
     float v = Input.GetAxis("Vertical");
@@ -43,16 +60,15 @@ public class PlayerMovement : MonoBehaviour
       moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
       controller.Move(moveDir * moveSpeed * Time.deltaTime);
     }
-  
-
-    // // Jumping
+    // Jumping
     if (Input.GetButtonDown("Jump") && playerIsGrounded)
+    {
       playerExtraVelocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-
-    // Add gravity
-    playerExtraVelocity.y += gravity * Time.deltaTime;
+      // Debug.Log("Jump Input!");
+    }
+    // Apply move to player
     controller.Move(playerExtraVelocity * Time.deltaTime);
+
 
 
 
@@ -60,24 +76,26 @@ public class PlayerMovement : MonoBehaviour
     animatior.SetFloat("speed", controller.velocity.magnitude);
     animatior.SetBool("isGrounded", playerIsGrounded);
     
-    Debug.DrawRay(transform.position+ new Vector3(0,1,0), transform.forward*5, Color.blue);
-    Debug.DrawRay(transform.position+ new Vector3(0,1,0), moveDir*5, Color.green);
-  }
 
 
-  void OnCollisionStay(Collision collisionInfo)
-  {
-    if (collisionInfo.gameObject.tag == "Terrain")
-    {
-      playerIsGrounded = true;
-    }
+    // Debug-Stuff
+    Debug.DrawRay(transform.position+ new Vector3(0,1,0), transform.forward*2, Color.blue);
+    Debug.DrawRay(transform.position+ new Vector3(0,1,0), moveDir*2, Color.green);
+    Debug.DrawRay(transform.position+ new Vector3(0,1,0), playerExtraVelocity, Color.white);
   }
-  void OnCollisionExit(Collision collisionInfo)
+  void OnDrawGizmos()
   {
-    if (collisionInfo.gameObject.tag == "Terrain")
-    {
-      playerIsGrounded = false;
-    }
+    // visualisation of Ground Collider
+    float radius = controller.radius * 0.9f;
+    Vector3 pos = transform.position + Vector3.up*(radius*0.5f);
+    Gizmos.color = Color.yellow;
+    Gizmos.DrawWireSphere(pos, radius);
+  }
+
+  // Getters
+  public bool getPlayerGrounded()
+  {
+    return playerIsGrounded;
   }
 
 }
